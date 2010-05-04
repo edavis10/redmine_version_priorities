7e90aa73542626db7b59c40fd4de7c93e04f7bc0
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-03 19:41:54 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_version_priorities do
+  name 'Redmine Version Priorities plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
