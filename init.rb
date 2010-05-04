42211c5b2b653c3c637656ff4ad1d3c1015144c5
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-03 19:49:20 -0700

    [#3981] Fixing docs and init.rb.

--- init.rb
+++ init.rb
@@ -1,8 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_version_priorities do
-  name 'Redmine Version Priorities plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Version Priorities'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-kanban'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Allows versions to be prioritized'
+  version '0.1.'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
