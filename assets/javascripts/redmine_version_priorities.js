5eb3492ef6ad26dd69f8dc4e3c2f8925b1faba7d
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-04 19:54:12 -0700

    [#3982] Connect the lists as sortable UIs.

--- assets/javascripts/redmine_version_priorities.js
+++ assets/javascripts/redmine_version_priorities.js
@@ -1,1 +1,29 @@
+jQuery(function($) {
+  $("#ajax-indicator").ajaxStart(function(){ $(this).show();  });
+  $("#ajax-indicator").ajaxStop(function(){ $(this).hide();  });
 
+  attachSortables = function() {
+    $('#prioritized-versions ol').sortable({
+      cancel: 'a',
+      connectWith: ["#unprioritized-versions ul"],
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true,
+      update: function (event, ui) {
+        alert('saved');
+      }
+    });
+
+    $('#unprioritized-versions ul').sortable({
+      cancel: 'a',
+      connectWith: ["#prioritized-versions ol"],
+      placeholder: 'drop-accepted',
+      dropOnEmpty: true,
+      update: function (event, ui) {
+        alert('saved');
+      }
+    });
+
+  },
+
+  attachSortables();
+});
