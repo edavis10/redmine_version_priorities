66a6722502b5fe54d186bbb6e27c0b2538577aa6
Eric Davis <edavis@littlestreamsoftware.com>
2010-05-04 20:07:53 -0700

    [#3982] Hooked up the frontend code.

--- assets/javascripts/redmine_version_priorities.js
+++ assets/javascripts/redmine_version_priorities.js
@@ -8,8 +8,14 @@ jQuery(function($) {
       connectWith: ["#unprioritized-versions ul"],
       placeholder: 'drop-accepted',
       dropOnEmpty: true,
+      // Allow drag and drop inside the list
       update: function (event, ui) {
-        alert('saved');
+        if (ui.sender == null && event.target == this) {
+          saveOrder();
+        }
+      },
+      receive: function (event, ui) {
+        saveOrder();
       }
     });
 
@@ -17,12 +23,28 @@ jQuery(function($) {
       cancel: 'a',
       connectWith: ["#prioritized-versions ol"],
       placeholder: 'drop-accepted',
-      dropOnEmpty: true,
-      update: function (event, ui) {
-        alert('saved');
-      }
+      dropOnEmpty: true
     });
 
+  },
+
+  saveOrder = function() {
+    var data = $('#prioritized-versions ol').sortable('serialize')
+
+    $.ajaxQueue.put('version_priorities.js', {
+      data: addAuthenticityToken(data),
+      success: function(response) {
+        $('#prioritized-versions').html(response);
+      },
+      error: function(response) {
+        $("div.error").html("Error saving lists.  Please refresh the page and try again.").show();
+
+      }
+    });
+  },
+
+  addAuthenticityToken = function(data) {
+    return data + '&authenticity_token=' + encodeURIComponent(window._token);
   },
 
   attachSortables();
